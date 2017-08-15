dfb89116a1f8c8094709db1cdbdeddb45f39ab4e
BruceCutler <bicutler10@gmail.com>
2017-08-15 16:51:24 -0400

    GDE-1161 Updating nxlog version in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name             'nxlog'
-version          '0.0.1'
+version          '0.8.8'
 
 description      'Grasshopper fork of the nxlog cookbook. Installs and configures NXLog_ce on Linux or Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
